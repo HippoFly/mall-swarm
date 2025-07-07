f9d54884ae2d5132c698668ec9a354f4576238d8
HippoFly <hippomayfly@gmail.com>
2025-07-07 20:39:37 +0800

    fix(mall-portal)：解决移动端的CORS问题

---
+++ mall-portal/src/main/java/com/macro/mall/portal/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.macro.mall.portal.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 全局跨域配置
+ * Created by macro on 2021/12/24.
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
