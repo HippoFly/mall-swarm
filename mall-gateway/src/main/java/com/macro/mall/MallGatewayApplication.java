fdb72ea0200df537c386716a0ef92b95af1934ce
HippoFly <hippomayfly@gmail.com>
2025-07-07 16:36:32 +0800

    update：gateway使用环境变量替换IP

--- mall-gateway/src/main/java/com/macro/mall/MallGatewayApplication.java
+++ mall-gateway/src/main/java/com/macro/mall/MallGatewayApplication.java
@@ -9,6 +9,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class MallGatewayApplication {
 
     public static void main(String[] args) {
+        // 检测环境变量是否加载
+        String nacosAddr = System.getenv("NACOS_ADDR");
+        System.out.println("环境变量 NACOS_ADDR: " + (nacosAddr != null ? nacosAddr : "未设置，使用默认值"));
+
         SpringApplication.run(MallGatewayApplication.class, args);
     }
 
